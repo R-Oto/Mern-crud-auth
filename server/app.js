d32050f1954b7e7cd405227581c9d50f3e564937
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-05 22:06:30 +0400

    coonected to mongo

--- server/app.js
+++ server/app.js
@@ -1,5 +1,17 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+dotenv.config()
 
 const app = express()
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server started'))
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect(process.env.ATLAS_URI).then(()=>{
+    console.log('Connected')
+    app.listen(PORT, () => console.log('Server started'))
+})
