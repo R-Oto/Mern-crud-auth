8b4a5a7d2112b6711031996b631b50aa7ddcf185
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-06 13:03:12 +0400

    register and login

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,7 +1,47 @@
-export const registerUser = async (req,res) => {
+import User from "../models/usersModel.js";
+import bcryptjs from "bcryptjs";
 
-}
+export const registerUser = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ error: "All fields are required" });
+    }
 
-export const loginUser = async (req,res) => {
+    try {
+        const exists = await User.findOne({ email });
+        if (exists) {
+            return res.status(400).json({ error: "Email is already used!" }); // Change to 400 for user error
+        }
 
-}
+        const salt = await bcryptjs.genSalt();
+        const hashed = await bcryptjs.hash(password, salt);
+
+        const user = await User.create({ email, password: hashed });
+        res.status(201).json({ message: "User created" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ error: "All fields are required" });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Email not found" }); // Change message for clarity
+        }
+
+        const match = await bcryptjs.compare(password, user.password);
+        if (!match) {
+            return res.status(400).json({ error: "Incorrect password" });
+        }
+
+        res.status(200).json({ email });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
--- server/models/usersModel.js
+++ server/models/usersModel.js
@@ -1,5 +1,19 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+}, {
+    timestamps: true
 })
+
+const userModel = mongoose.model("User", UserSchema)
+
+export default userModel;
