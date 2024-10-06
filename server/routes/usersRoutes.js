ba808a42ac6aee27a1e0edde6462e7eb876da0de
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-06 15:22:04 +0400

    mern

--- server/routes/usersRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { loginUser, registerUser } from '../controllers/usersController.js'
-const router = express.Router()
-
-router.post("/", registerUser)
-
-router.post("/login", loginUser)
-
-export default router;
